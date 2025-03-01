e2b139e980751feb9f022d16d5ef562ef6cd7bcd
you@example.com <you@example.com>
2025-03-01 20:04:39 +1100

    eslintrc.json and updated next.config.mjs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
+// .eslintrc.js
 module.exports = {
-    extends: 'next/core-web-vitals',
+    root: true,
+    extends: ['next/core-web-vitals'],
     rules: {
       'react/no-unescaped-entities': 'off',
+      'import/no-anonymous-default-export': 'off'
+    },
+    parserOptions: {
+      babelOptions: {
+        presets: [require.resolve('next/babel')],
+      },
     },
   };
